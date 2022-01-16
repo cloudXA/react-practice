511b45862023b97599b6458c7745c1d99be57e50
cloudXA <zming794@gmail.com>
2022-01-16 16:49:59 +0800

    使用react api对jsx编译后节点进行管理

--- src/components/JsxPractice.jsx
+++ src/components/JsxPractice.jsx
@@ -30,7 +30,60 @@ export default class JsxPractice extends Component {
                 <button onClick={ ()=> console.log( this.render() ) } >打印render后的内容</button>
             </div>
         )
-        return reactElement;
+        console.log(reactElement.props.children, 'react ')
+        const { children } = reactElement.props;
+        /**
+         * 1. children扁平化处理
+         */
+         const newElement = reactElement.props.children.reduce((current, pre) => {
+            if(Array.isArray(pre)) {
+                return current.concat(pre)
+            } 
+            current.push(pre)
+            return current;
+         }, [])
+
+         const flatChildren = React.Children.toArray(children);
+
+         /**
+         * 2. 干掉children中文本类型节点
+         */
+        const temElement = newElement.reduce((current, pre) => {
+            if(typeof pre === 'string') {
+                return current
+            } 
+            current.push(pre)
+            return current;
+         }, [])
+
+
+         const newChildren = [];
+         React.Children.forEach(flatChildren, (item) => {
+             if(React.isValidElement(item)) newChildren.push(item)
+         })
+
+
+         /**
+          * 3. 向children最后插入say goodbye 元素
+          */
+         const sayGoodbye = React.createElement('div', null, 'goodbye')
+         temElement.push(sayGoodbye);
+
+
+         const lastChildren = React.createElement('div', { className: 'last' }, 'goodbye')
+         newChildren.push(lastChildren);
+
+         /**
+          * 4. 克隆新的元素节点并渲染
+          */
+         
+
+        //  console.log(temElement, 'tem')
+        //  将newChildren插入到reactElement的props中
+         const newReactElement = React.cloneElement(reactElement, {}, ...newChildren)
+
+
+        return newReactElement;
     }
     render() {
         return (
@@ -44,7 +97,18 @@ export default class JsxPractice extends Component {
 /**
  * jsx编译后结构认识，对jsx编译后的react.element进行一系列操作，达到理想化的目的，熟悉react api使用
  * 1. 将children扁平化处理，将数组类型的子节点打开；
+ *       const flatChildren = React.Children.toArray(children);
  * 2. 干掉children中文本类型节点
+ *       const newChildren = [];
+         React.Children.forEach(flatChildren, (item) => {
+             if(React.isValidElement(item)) newChildren.push(item)
+         })
  * 3. 向children最后插入say goodbye 元素
+         const lastChildren = React.createElement('div', { className: 'last' }, 'goodbye')
+         newChildren.push(lastChildren);
  * 4. 克隆新的元素节点并渲染
+         //  将newChildren插入到reactElement的props中
+         const newReactElement = React.cloneElement(reactElement, {}, ...newChildren)
+ * 
+ * 
  */
